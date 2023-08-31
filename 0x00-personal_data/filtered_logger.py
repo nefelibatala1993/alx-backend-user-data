5df092a9bdc47af815de7763b9abede097d27f67
nefelibatala1993 <leuela1993@gmail.com>
2023-08-31 20:28:45 +0300

    add the second task with the curstom formater

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """Defines a function called filter_datum that returns a list of
 obfuscated log messages."""
+import logging
 import re
 from typing import List
 
@@ -12,3 +13,24 @@ def filter_datum(fields: List[str], redaction: str,
         pattern = fr'({ field.strip() }=)([^{separator}]+){separator}'
         message = re.sub(pattern, r'\1' + redaction + separator, message)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        self.fields = fields
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+
+    def format(self, record: logging.LogRecord) -> str:
+        """Defines a custom formatter with the redacted value and
+        the redactor and the separator"""
+        msg = super(RedactingFormatter, self).format(record)
+        redacted_msg = filter_datum(self.fields, self.REDACTION,
+                                    msg, self.SEPARATOR)
+        return redacted_msg
