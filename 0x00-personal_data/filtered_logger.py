c6e10ee2ea675a81ef6162be393feba155082ab3
nefelibatala1993 <leuela1993@gmail.com>
2023-08-31 15:39:33 +0300

    add the first task for the project

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python3
 """Defines a function called filter_datum that returns a list of
 obfuscated log messages."""
-from typing import List
+import re
 
 
-def filter_datum(fields: List[str], redaction: str,
-                 message: str, separator: str):
-    ...
+def filter_datum(fields, redaction, message, separator):
+    """filters through a message that is supplied to it, and
+    it redacts the associated field with it"""
+    for field in fields:
+        pattern = fr'({ field.strip() }=)([^{separator}]+){separator}'
+        message = re.sub(pattern, r'\1' + redaction + separator, message)
+    return message
