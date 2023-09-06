e073f5812c57d52973bacd3c004c7998fa65e2ae
nefelibatala1993 <leuela1993@gmail.com>
2023-09-06 20:06:01 +0300

    pass the pycodestyle and also made the file executable

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -26,7 +26,7 @@ def stats() -> str:
     return jsonify(stats)
 
 
-@app_views.route('/unauthorized', methods=['GET'], strict_slashes= False)
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
 def unauthorzied_access() -> str:
     """GET /api/v1/unauthorized"""
     abort(401)
@@ -36,3 +36,4 @@ def unauthorzied_access() -> str:
 def forbidden_access() -> str:
     """GET /api/v1/forbidden"""
     abort(403)
+    
