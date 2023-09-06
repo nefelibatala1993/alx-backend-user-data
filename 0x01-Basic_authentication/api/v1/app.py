6cc71134571abc583ea50e9d416d6a0ce36eb156
nefelibatala1993 <leuela1993@gmail.com>
2023-09-06 19:59:44 +0300

    add the type for the return type of the error handler for the 401 http code

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -6,7 +6,6 @@ from os import getenv
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
-import os
 from api.v1.auth.auth import Auth
 from api.v1.auth.basic_auth import BasicAuth
 
@@ -17,7 +16,7 @@ CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
 
 
-auth_type = os.getenv('AUTH_TYPE')
+auth_type = getenv('AUTH_TYPE')
 if auth_type == 'auth':
     auth = Auth()
 elif auth_type == 'basic_auth':
@@ -43,7 +42,7 @@ def not_found(error) -> str:
 
 
 @app.errorhandler(401)
-def unauthorized_access(error):
+def unauthorized_access(error) -> str:
     """ Unauthorized access handler
     """
     return jsonify({ "error": "unauthorized" }), 401
