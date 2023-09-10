13e5eac2782064f7d0cb59e570c11818976294c9
nefelibatala1993 <leuela1993@gmail.com>
2023-09-10 13:15:44 +0300

    add the logic for the require_auth function

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -8,7 +8,16 @@ class Auth:
     """Authentication class for authentication of users to the API"""
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """Require authentication for all routes except excluded_paths"""
-        return False
+        if path is None or excluded_paths is None or len(excluded_paths) == 0:
+            return True
+        
+        # make the path slash tolerant in the search
+        if path[-1] != '/':
+            path += '/'
+        
+        if path in excluded_paths:
+            return False
+        return True
     
     def authorization_header(self, request=None) -> str:
         """Return the value of the header request Authorization"""
