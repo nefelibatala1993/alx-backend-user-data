a29ec01eea63ff2de1327a7d495d293f60a3a088
nefelibatala1993 <leuela1993@gmail.com>
2023-09-10 16:45:01 +0300

    add the decode method

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 """Defines the BasicAuth Class"""
+import base64
+import binascii
 from api.v1.auth.auth import Auth
 
 
@@ -18,3 +20,18 @@ class BasicAuth(Auth):
         if _list[0] != 'Basic':
             return None
         return _list[1]
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """Decodes the base64 authorization header to a normal string"""
+        if base64_authorization_header is None:
+            return None
+
+        if not isinstance(base64_authorization_header, str):
+            return None
+
+        try:
+            decoded_bytes = base64.b64decode(base64_authorization_header)
+        except binascii.Error as e:
+            return None
+        return decoded_bytes.decode('utf-8')
