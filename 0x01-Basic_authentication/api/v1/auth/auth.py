e739dc988445409497f557afef99f86a7d425ee1
nefelibatala1993 <leuela1993@gmail.com>
2023-09-10 13:05:01 +0300

    add the Auth class

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""Authentication for the API"""
+from flask import request
+from typing import List
+
+
+class Auth:
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """Require authentication for all routes except excluded_paths"""
+        return False
+    
+    def authorization_header(self, request=None) -> str:
+        """Return the value of the header request Authorization"""
+        return None
+    
+    def current_user(self, request=None) -> str:
+        """Returns the current use that is authenticated"""
+        return None
