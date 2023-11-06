b05aec032241b5deef6e57331b5139ee293f0d8c
nefelibatala1993 <leuela1993@gmail.com>
2023-11-06 20:51:40 +0300

    add the base auth class for user authentication

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""Defines the Auth class that handles the
+the authentication of users for the api
+"""
+from flask import request
+
+
+class Auth:
+    """Authentication class for users
+    """
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """Checks if the path requires authenication.
+        """
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """Gets the authorization header from the request header
+        """
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """Gets the current user that is authenticated
+        """
+        return None
