5ff3f83929fbbca599eb1e020e3c113ea6bd90d3
nefelibatala1993 <leuela1993@gmail.com>
2023-11-06 20:40:54 +0300

    add the third task of the project

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -31,3 +31,10 @@ def unauthorized() -> None:
     """Unauthorized route
     """
     abort(401)
+
+
+@app_views.route('/forbidden', strict_slashes=False)
+def forbidden() -> None:
+    """Resource forbidden route tester
+    """
+    abort(403)
