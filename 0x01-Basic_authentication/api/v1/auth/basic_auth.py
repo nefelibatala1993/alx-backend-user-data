bb7c257309b0894ffc278530d447795fe7d0de6a
nefelibatala1993 <leuela1993@gmail.com>
2023-11-07 07:28:21 +0300

    add the extract_base64 string function to the BasicAuth function

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -5,4 +5,17 @@ from api.v1.auth.auth import Auth
 
 class BasicAuth(Auth):
     """Definition for the Basic Authentication"""
-    pass
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """Extract the base64 string from the authorization header"""
+        if authorization_header is None:
+            return None
+
+        if not isinstance(authorization_header, str):
+            return None
+
+        arr_header = authorization_header.split()
+        if arr_header[0] != 'Basic':
+            return
+
+        return arr_header[1]
