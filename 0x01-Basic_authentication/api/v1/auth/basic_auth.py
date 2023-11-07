2d05712792fe789545ca068da4ed52e897ea440b
nefelibatala1993 <leuela1993@gmail.com>
2023-11-07 07:19:32 +0300

    Update the BasicAuth Clas

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 """Defines the BasicAuth class"""
+from api.v1.auth.auth import Auth
 
 
-class BasicAuth:
+class BasicAuth(Auth):
     """Definition for the Basic Authentication"""
     pass
