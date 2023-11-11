4675ff615b79985f0aeda242d7054ab762ed386f
nefelibatala1993 <leuela1993@gmail.com>
2023-11-11 20:38:56 +0300

    finish the second task of the project

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Defines the SessionAuth class"""
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """Defines the session authentication class that uses cookies"""
+    pass
