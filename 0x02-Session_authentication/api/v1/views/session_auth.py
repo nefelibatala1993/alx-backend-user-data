89edaf9047f8d9aded6a07b9f5f3e9a8bf689eae
nefelibatala1993 <leuela1993@gmail.com>
2023-11-12 19:15:17 +0300

    add the logic for the session authentication class

---
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+"""Module for Session Authentication Views
+"""
+from api.v1.views import app_views
+from flask import jsonify, request
+from models.user import User
+from os import getenv
+
+
+@app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
+def login() -> str:
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    if email is None or email == "":
+        return jsonify({"error": "email missing"}), 400
+
+    if password is None or password == "":
+        return jsonify({"error": "password missing"}), 400
+
+    try:
+        users = User.search({"email": email})
+    except Exception:
+        return None
+
+    if len(users) == 0:
+        return jsonify({"error": "no user found for this email"}), 404
+
+    for user in users:
+        if user.is_valid_password(password):
+            from api.v1.app import auth
+            session_id = auth.create_session(user.id)
+            SESSION_NAME = getenv('SESSION_NAME')
+
+            response = jsonify(user.to_json())
+            response.set_cookie(SESSION_NAME, session_id)
+            return response
+
+    return jsonify({"error": "wrong password"}), 401
