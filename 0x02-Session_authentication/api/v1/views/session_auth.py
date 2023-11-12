3c4222b90419f2c2a300a51d04380df44e1b27cd
nefelibatala1993 <leuela1993@gmail.com>
2023-11-12 21:08:53 +0300

    add the docs for the login route

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -9,6 +9,8 @@ from os import getenv
 
 @app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
 def login() -> str:
+    """Defines the login route for the user to login
+    """
     email = request.form.get('email')
     password = request.form.get('password')
 
