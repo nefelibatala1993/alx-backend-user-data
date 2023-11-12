f9cf49ccc225b8f7ab0b33af34a2994589a5a75b
nefelibatala1993 <leuela1993@gmail.com>
2023-11-12 07:36:22 +0300

    created the create_session funciton in the SessionAuth Class

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,8 +1,18 @@
 #!/usr/bin/env python3
 """Defines the SessionAuth class"""
 from api.v1.auth.auth import Auth
+from uuid import uuid4
 
 
 class SessionAuth(Auth):
     """Defines the session authentication class that uses cookies"""
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """Creates a session ID for a user_id"""
+        if user_id is None or not isinstance(user_id, str):
+            return None
+
+        session_id = uuid4()
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
