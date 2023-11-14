0a32eacbb4325bcb97c07b19c4468a7f00da6b31
nefelibatala1993 <leuela1993@gmail.com>
2023-11-14 16:41:11 +0300

    Update the User model class to correct fields

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -10,7 +10,7 @@ class User(Base):
     __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
     hashed_pwd = Column(String(250), nullable=False)
     session_pwd = Column(String(250), nullable=True)
     rest_token = Column(String(250), nullable=True)
