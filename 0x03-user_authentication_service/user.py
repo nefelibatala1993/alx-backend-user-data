569477caf9e1bea7217f719b5d389b9956656175
nefelibatala1993 <leuela1993@gmail.com>
2023-11-14 16:50:02 +0300

    Update User class model to include session_id and reset_token columns

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-"""Defines the User class which will model users in the DB"""
+"""Defines the User class which will model users in the DB
+"""
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -8,11 +9,12 @@ Base = declarative_base()
 
 
 class User(Base):
-    """Defines the model for the user in the DB"""
+    """Defines the model for the user in the DB
+    """
     __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
-    session_id = Column(String(250), nullable=True)
-    rest_token = Column(String(250), nullable=True)
+    session_id = Column(String(250))
+    reset_token = Column(String(250))
