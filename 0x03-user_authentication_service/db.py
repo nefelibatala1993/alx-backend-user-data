151ce8a9228d1993c40d3b4130dc9f1091b103b0
nefelibatala1993 <leuela1993@gmail.com>
2023-11-14 19:11:50 +0300

    Add return type hint for add_user method

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -30,7 +30,7 @@ class DB:
             self.__session = DBSession()
         return self.__session
 
-    def add_user(self, email: str, hashed_password: str):
+    def add_user(self, email: str, hashed_password: str) -> User:
         """Adds a user to the DB
         """
         user = User(email=email, hashed_password=hashed_password)
